96013becb5ddef6eb9d5de2e11e10d56d4da0bef
csinrn <csinrn24@gmail.com>
2019-01-25 12:29:17 +0800

    second commit

---
+++ Assets/Password.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//第一個是getEgg password  後typeNum個是龍的品種
+[System.Serializable]
+public class PasswordP
+{
+    public Password[] pwarray;
+}
+
+
+[System.Serializable]
+public class Password
+{
+    public string key;
+    public string content;
+}
